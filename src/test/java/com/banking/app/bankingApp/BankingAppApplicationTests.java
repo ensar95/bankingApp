18929f5cff0662e94d2e0d329511bbda63272205
ensar95 <ensarskopljak@gmail.com>
2020-02-07 19:43:20 +0100

    added test for get all users and create user

--- src/test/java/com/banking/app/bankingApp/BankingAppApplicationTests.java
+++ src/test/java/com/banking/app/bankingApp/BankingAppApplicationTests.java
@@ -2,12 +2,16 @@ package com.banking.app.bankingApp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class BankingAppApplicationTests {
 
 	@Test
 	void contextLoads() {
+
 	}
 
 }
